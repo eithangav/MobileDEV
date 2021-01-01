63ad930c5f7b08db5a33772fcd72c04af6df65ac
Eithan Gavrieli <eitangav@gmail.com>
2021-01-01 11:51:20 +0200

    EX05 is done

--- EX05/server.js
+++ EX05/server.js
@@ -3,26 +3,28 @@ let bodyParser = require('body-parser');
 let url = require('url');
 let path = require('path');
 let fs = require('fs');
-let os = require('os');
-
 let app = express();
 let port = 8080;
 
-//parsing
-//app.use(bodyParser.json());
-
-//request: show all current tasks
+//request by 'files/filename'
+//prints the file's content into the browser
 app.get('/files/*', (req, res) => {
+	//get url
 	let urlTxt = req.url;
+	//parse url
 	let urlTxtParsed = url.parse(urlTxt);
+	//extract filename
 	let fileName = path.basename(urlTxtParsed.pathname);
-	//read the file
-	//let fileTxt = fs.readFileSync(fileName, 'utf8');
-	let tmp = path.join(os.tmpdir(), fileName);
-	let outstream = fs.createWriteStream(tmp);
-  	res.pipe(outstream);
+	//create file's full path
+	let filePath = path.dirname(require.main.filename) + '\\' + fileName;
+	//create a stream with files content
+	let outstream = fs.createReadStream(filePath);
+	//pipe it out
+	outstream.pipe(res);
+
 });
 
+//listener
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
 });
